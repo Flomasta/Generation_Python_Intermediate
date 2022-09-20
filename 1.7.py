e5191cc40cb1e69d0d57ed38ce50035f1a4530ba
flomasta <zatevi@mail.ru>
2022-09-20 11:10:19 +0300

    8th task resolved

--- 1.7.py
+++ 1.7.py
@@ -1,3 +1,10 @@
 # используем запятую в качестве разделителя тысяч
 
 print('{:,}'.format(int(input())))
+
+n=int(input())
+k=int(input())
+res=0
+for i in range(1, n+1):
+    res = (res + k) % i
+print(res + 1)

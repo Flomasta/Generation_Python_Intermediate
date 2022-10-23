3d223277d65a11eaef92be167dfec4d4e6eb6fa5
flomasta <zatevi@mail.ru>
2022-10-24 00:17:12 +0300

    part 4.7 Matrix 2nd task without numpy

--- 4.7.2.py
+++ 4.7.2.py
@@ -1,10 +1,24 @@
-import numpy as np
+# import numpy as np
+#
+# a, b = list(map(int, input().split()))
+# mtx_1 = [list(map(int, input().split())) for i in range(a)]
+# input()
+# c, d = list(map(int, input().split()))
+# mtx_2 = [list(map(int, input().split())) for i in range(c)]
+#
+# total = np.dot(mtx_1, mtx_2)
+# [print(*i) for i in total]
+
 
 a, b = list(map(int, input().split()))
 mtx_1 = [list(map(int, input().split())) for i in range(a)]
 input()
 c, d = list(map(int, input().split()))
 mtx_2 = [list(map(int, input().split())) for i in range(c)]
+mtx_3 = [[0] * d for _ in range(a)]
 
-total = np.dot(mtx_1, mtx_2)
-[print(*i) for i in total]
+for i in range(a):
+    for j in range(d):
+        for q in range(c):
+            mtx_3[i][j] += mtx_1[i][q] * mtx_2[q][j]
+[print(*i) for i in mtx_3]

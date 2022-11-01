53fbb61163f3816695ac6bedbacbe57ff3b68c6d
flomasta <zatevi@mail.ru>
2022-11-01 23:08:46 +0300

    I forgot to pass chapter 8.4 :)

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-set1 = {'a', 't', 'f','p'}
-set2 = {'a', 't', 'f'}
-
-print(set1 - set2)
+n, m = set(input()), set(input())
+mask = {*range(10)}
+res = {*set(map(int,n)), *set(map(int,m))}
+print('YES' if mask == res else 'NO')
 
 # Наполните множество set1 содержимым так, чтобы программа вывела {'p'}.

c3363c988e9987d1e8add75de462c4c6b871e253
flomasta <zatevi@mail.ru>
2022-11-09 11:42:57 +0300

    Fractions and Complex module Vol 13.2 and Vol 13.3

---
+++ 13.3.1.py
@@ -0,0 +1,5 @@
+# put your python code here
+n, m = complex(input()), complex(input())
+print(f'{n} + {m} = {n + m}')
+print(f'{n} - {m} = {n - m}')
+print(f'{n} * {m} = {n * m}')
--- test.py
+++ test.py
@@ -1,8 +1,9 @@
-from random import randrange
+def mean(*args):
+    res = [i for i in list(args) if type(i) == int or type(i) == float]
+    if len(res) > 0:
+        return sum(res) / len(res)
+    else:
+        return len(res)
 
-counter = 0
-i = randrange(256)
-while i != 0:
-    i = randrange(256)
-    counter += 1
-print(i, counter)
+
+print(mean(1.5, True, ['stepik'], 'beegeek', 2.5))
